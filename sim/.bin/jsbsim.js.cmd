1084365128b5c747b7b567c2120badfe76328f0e
Eric Burlingame <eric@burlingames.com>
2016-07-06 18:10:39 -0700

    Cleaned up and organized

--- sim/.bin/jsbsim.js.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\jsbsim.js\bin\jsbsim-cli" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\jsbsim.js\bin\jsbsim-cli" %*
-)
